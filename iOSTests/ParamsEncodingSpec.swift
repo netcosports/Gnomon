32620d33272bd627d60322e30bc4f7d0f2092d62
Vladimir Burdukov <chippcheg@gmail.com>
2017-07-31 17:25:46 +0300

    Added travis integration

--- iOSTests/ParamsEncodingSpec.swift
+++ iOSTests/ParamsEncodingSpec.swift
@@ -65,6 +65,20 @@ class ParamsEncodingSpec: XCTestCase {
     }
   }
 
+  func testInnerDictionaryInArray() {
+    do {
+      let request: Request<SingleResult<String>> = try RequestBuilder().setURLString("http://httpbin.org/get")
+        .build()
+      let params: [String: Any] = ["key1": "value1", "key2": "value2",
+                                   "key3": [["inKey1": "inValue1", "inKey2": "inValue2"]]]
+      let expected = "http://httpbin.org/get?key1=value1&key2=value2&key3%5B%5D%5BinKey1%5D=inValue1&" +
+        "key3%5B%5D%5BinKey2%5D=inValue2"
+      expect(try prepareURL(from: request, params: params).absoluteString).to(equal(expected))
+    } catch {
+      fail("\(error)")
+    }
+  }
+
   func testNumbers() {
     do {
       let request: Request<SingleResult<String>> = try RequestBuilder().setURLString("http://httpbin.org/get")
