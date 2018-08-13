1fe96b1330c1f9b047fd3a0d9860fa5511d26601
Vladimir Burdukov <vladimir_burdukov@epam.com>
2018-08-13 16:14:47 +0300

    Updated Xcode in travis configuration

--- scripts/destination.sh
+++ scripts/destination.sh
@@ -1,6 +1,6 @@
 case $1 in
-  ("iOSTests") SIMULATOR='NetcoTests/iPhone-7/iOS/11.3' ;;
-  ("tvOSTests") SIMULATOR='NetcoTests/Apple-TV-1080p/tvOS/11.3' ;;
+  ("iOSTests") SIMULATOR='NetcoTests/iPhone-7/iOS/11.4' ;;
+  ("tvOSTests") SIMULATOR='NetcoTests/Apple-TV-1080p/tvOS/11.4' ;;
   ("macOSTests") echo "platform=macOS,arch=x86_64"; exit 0 ;;
   (*) exit -1 ;;
 esac
